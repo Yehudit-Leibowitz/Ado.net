44c9e91338742ffbfc3f398f99ef77379c558142
Yehudit-Leibowitz <38214938334@mby.co.il>
2024-11-17 18:52:40 +0200

    Add project files.

---
+++ Ado.net/DataAccess.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.net
+{
+    public class DataAccess
+    {
+        public int AddCategory (string connectionString)
+        {
+            string CategoryName;
+            Console.WriteLine("insert category name");
+            CategoryName = Console.ReadLine();
+
+        
+            string query = "INSERT INTO Category (CategoryName)" + "VALUES(@CategoryName)";
+            using (SqlConnection connection = new SqlConnection(connectionString)) 
+            using (SqlCommand cmd =new SqlCommand(query, connection)) 
+
+
+            {
+                cmd.Parameters.Add("@CategoryName", System.Data.SqlDbType.VarChar, 10).Value = CategoryName;
+                connection.Open();
+                int rowAffected = cmd.ExecuteNonQuery();
+                connection.Close();
+                return rowAffected;
+
+
+            }
+        
+        }
+
+
+        public int AddProduct(string connectionString)
+        {
+            string Catedory_Id, ProductName, ProductDescription, Price, PictuerPath;
+  
+      
+            Console.WriteLine("insert Catedory_Id name");
+            Catedory_Id = Console.ReadLine();
+            Console.WriteLine("insert ProductName name");
+            ProductName = Console.ReadLine();
+            Console.WriteLine("insert ProductDescription name");
+            ProductDescription = Console.ReadLine();
+            Console.WriteLine("insert Price ");
+
+            Price = Console.ReadLine();
+            Console.WriteLine("insert Picture path");
+            PictuerPath = Console.ReadLine();
+
+            string query = "INSERT INTO Product (Catedory_Id, ProductName, ProductDescription, Price, PictuerPath)" + "VALUES(@Catedory_Id, @ProductName, @ProductDescription, @Price, @PictuerPath)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+
+
+            {
+                cmd.Parameters.Add("@Catedory_Id", System.Data.SqlDbType.Int).Value = Catedory_Id;
+                cmd.Parameters.Add("@ProductName", System.Data.SqlDbType.VarChar, 20).Value = ProductName;
+                cmd.Parameters.Add("@ProductDescription", System.Data.SqlDbType.VarChar, 100).Value = ProductDescription;
+                cmd.Parameters.Add("@Price", System.Data.SqlDbType.Int).Value = Price;
+                cmd.Parameters.Add("@PictuerPath", System.Data.SqlDbType.VarChar, 10).Value = PictuerPath;
+                connection.Open();
+                int rowAffected = cmd.ExecuteNonQuery();
+                connection.Close();
+                return rowAffected;
+
+
+            }
+
+        }
+
+
+
+        public void getAllProduct(string connectionString)
+        {
+            
+
+            string query = "select * from Product";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            { 
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}", reader[0], reader[1], reader[2], reader[3], reader[4], reader[5]);
+                        }
+                        reader.Close();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                Console.ReadLine();
+
+
+            }
+
+        }
+
+        public void getAllCategory(string connectionString)
+        {
+
+
+            string query = "select * from Product";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("\t{0}\t{1}", reader[0], reader[1]);
+                        }
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                Console.ReadLine();
+
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Ado.net/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Remoting.Messaging;
+using System.Security.Permissions;
+using System.Windows.Forms;
+
+namespace Ado.net
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //string connectionString = "Data Source = SRV2\PUPILS; Initial Catalog = api_ado; Integrated Security = True; Trust Server Certificate = True";
+            string connectionString = "data source=srv2\\pupils;initial catalog=api_ado;Integrated Security=SSPI;Persist Security Info=False;TrustServerCertificate=true";
+
+            DataAccess dataAccess = new DataAccess();
+       
+            //add category 
+            dataAccess.AddCategory(connectionString);
+            DialogResult answerOfCategory = MessageBox.Show("Do you want to continue it?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            while (answerOfCategory == DialogResult.Yes)
+            {
+                dataAccess.AddCategory(connectionString);
+                answerOfCategory = MessageBox.Show("Do you want to continue it?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            }
+            //add Product 
+            dataAccess.AddProduct(connectionString);
+            DialogResult answerOfProduct = MessageBox.Show("Do you want to continue it?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            while (answerOfProduct == DialogResult.Yes)
+            {
+                dataAccess.AddProduct(connectionString);
+                answerOfProduct = MessageBox.Show("Do you want to continue it?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            }
+            //get products
+            dataAccess.getAllProduct(connectionString);
+            //get category
+            dataAccess.getAllCategory(connectionString);
+
+        }
+    }
+}
